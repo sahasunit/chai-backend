a7dbd860c34f80f09925e55db3ec3cd5028d9d7e
Sunit Saha <coolsunit10@gmail.com>
2025-11-25 17:22:24 +1100

    connect to mongo atlas

---
+++ src/index.js
@@ -0,0 +1,24 @@
+//require('dotenv').config({path: './env'});
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path: './env'
+})
+
+
+
+connectDB();
+
+/*
+//IIFE function
+( async () => {
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+    } catch (error) {
+        console.error("Error: ", error);
+        throw err;
+    }
+
+})()
+*/
