1d391b961020cbbe09e9812b2a2ca2cce30c4b04
Sunit Saha <coolsunit10@gmail.com>
2025-12-02 12:23:30 +1100

    Add middleware login & logout controllers

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,6 +4,21 @@ import { User } from '../models/user.model.js';
 import { uploadOnloudinary } from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 
+const generateAccessAndRefreshTokens = async(userId) => {
+    try {
+        const user = await User.findById(userId);
+        const accessToken = user.generateAccessToken();
+        const refreshToken = user.generateRefreshToken();
+
+        user.refreshToken = refreshToken;
+        await user.save({ validateBeforeSave: false });  //save refreshToken in DB. validateBeforeSave is used to disable validation for reqd. fields like password
+        
+        return {accessToken, refreshToken};
+    } catch (error) {
+        throw new ApiError(500, "Something went wrong while generating access & refresh token");
+    }
+}
+
 const registerUser = asyncHandler( async (req,res) => {
     // get user details from frontend
     // validation - not empty
@@ -79,5 +94,88 @@ const registerUser = asyncHandler( async (req,res) => {
     );
 })
 
+const loginUser = asyncHandler( async (req, res) => {
+    // req body -> data
+    // check for username or email
+    // find the user from database
+    // Check password
+    // generate access and refresh token
+    // send secure cookies
 
-export { registerUser }
+    const { username, email, password } = req.body;
+
+    if (!username || !email) {
+        throw new ApiError(400, "username or password is required")
+    };
+
+    const user = await User.findOne({
+        $or: [{username}, {email}] //builtin mongoDB operation to check either for username or email
+    });
+
+    if (!user) {
+        throw new ApiError(404, "User not found")
+    };
+
+    const isPasswordValid = await user.isPasswordCorrect(password);
+
+    if (!isPasswordValid) {
+        throw new ApiError(401, "Invalid user credentials")
+    };
+
+    const {accessToken, refreshToken} = await generateAccessAndRefreshTokens(user._id);
+
+    const loggedInUser = await User.findById(user._id).select("-password, -refreshToken");
+
+    const options  = {
+        httpOnly: true,
+        secure: true
+    } //options created for cookies. httpOnly and secure is set to true to make sure the cookies are only modifioed in server and not on client side
+
+    return res
+    .status(200)
+    .cookie("accessToken", accessToken, options)
+    .cookie("refreshToken", refreshToken, options)
+    .json(
+        new ApiResponse(
+            200,
+            {
+                user: loggedInUser, accessToken, refreshToken
+            },
+            "User logged in successfully"
+        )
+    )
+});
+
+const logoutUser = asyncHandler(async(req, res) => {
+    await User.findByIdAndUpdate(
+        req.user._id,
+        {
+            $set: {
+                refreshToken: undefined
+            }
+        },
+        {
+            new: true
+        }
+    );
+
+    const options  = {
+        httpOnly: true,
+        secure: true
+    }
+
+    return res
+    .status(200)
+    .clearCookie("accessToken", accessToken, options)
+    .clearCookie("refreshToken", refreshToken, options)
+    .json(
+        new ApiResponse(
+            200,
+            {},
+            "User logged out"
+        )
+    )
+});
+
+
+export { registerUser, loginUser, logoutUser }
